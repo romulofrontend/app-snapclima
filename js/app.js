7318cc930694568ce99a3153841efb55efec39a6
Romulo Rosa <romulo.frontend@gmail.com>
2023-01-28 17:40:13 -0300

    js var com elementos com interacao e exibicao

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("Imersão Front-End");
+
+//Interação
+const citySearchInput = document.getElementById('city-search-input')
+const citySearchButton = document.getElementById('city-search-button')
+
+//Exibição
+const currentDate = document.getElementById('current-date')
+const cityName = document.getElementById('city-name')
+const weatherIcon = document.getElementById('weather-icon')
+const weatherDescription = document.getElementById('weather-description')
+const currentTemperature = document.getElementById('current-temperature')
+const windSpeed = document.getElementById('wind-speed')
+const feelsLikeTemperature = document.getElementById('feels-like-temperature')
+const currentHumidity = document.getElementById('current-humidity')
+const sunsetTime = document.getElementById('sunset-time')
+const sunriseTime = document.getElementById('sunrise-time')
+
